fab1c251d45ae95375b26a7a508df3efb6f8e0b3
许凌绝 <xulj09@vanke.com>
2023-12-25 17:18:25 +0800

    1.增加日志示例 2.增加根据不同环境启动的示例

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,11 +1,14 @@
-import uvicorn
+import logging, uvicorn
 from fastapi import Depends, FastAPI
-from app.api.deps import get_query_token, get_token_header
+
 from app.api.api_v1 import users, items
+from app.api.deps import get_query_token, get_token_header
 from .internal import admin
+from .settings import LOGGING_CONF
+
+logging.config.dictConfig(LOGGING_CONF)
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
-
 app.include_router(users.router)
 app.include_router(items.router)
 app.include_router(
@@ -15,12 +18,6 @@ app.include_router(
     dependencies=[Depends(get_token_header)],
     responses={418: {"description": "I'm a teapot"}},
 )
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
